bcca20f56f8fc275361fed0304b47c839dd4a038
sambodjango <gringolalia@gmail.com>
2021-01-17 19:34:53 -0300

    deploy

---
+++ precache-manifest.e58e9149ae5022ea54e14c077d4d18e8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417838a6292f64b32107",
+    "url": "/css/app.5e63c231.css"
+  },
+  {
+    "revision": "1052aec26af3b44483ad",
+    "url": "/css/chunk-vendors.6b88b602.css"
+  },
+  {
+    "revision": "39083cdef834fe0d7ee39e0272d1aa76",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.39083cde.woff"
+  },
+  {
+    "revision": "d960b795b343e35150c1a0aa3c38c16b",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.d960b795.woff2"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "/img/logo.8c4120b4.png"
+  },
+  {
+    "revision": "ec885583295e5abd38e107ea9ac4eb4e",
+    "url": "/img/nyc-city-hall-station.ec885583.jpg"
+  },
+  {
+    "revision": "5333754f3dbddd51b4d9e9fc71c9e482",
+    "url": "/img/nyc-dynamo.5333754f.jpg"
+  },
+  {
+    "revision": "1777cb2c98770419bbea08cc84467b46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f66f20169816ca9739e",
+    "url": "/js/about.e22d5b8f.js"
+  },
+  {
+    "revision": "417838a6292f64b32107",
+    "url": "/js/app.27692f2f.js"
+  },
+  {
+    "revision": "1052aec26af3b44483ad",
+    "url": "/js/chunk-vendors.7342d36f.js"
+  },
+  {
+    "revision": "60ef7e7daf5bf3fc0bca2965c973d16a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
